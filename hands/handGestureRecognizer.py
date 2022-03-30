020d95ffcd5febd85edb8a947771b3ef411fc355
Bardia Jedi <bardiajedi@gmail.com>
2022-03-30 16:31:26 +0200

    prefementce update

--- hands/handGestureRecognizer.py
+++ hands/handGestureRecognizer.py
@@ -2,10 +2,13 @@ import cv2 as cv
 import numpy as np
 import tensorflow as tf
 
+import mediapipe.python.solutions.hands as mpHands
+import mediapipe.python.solutions.drawing_utils as mpdUtils
+
+
 from handProcessor import HandProcessor
 
-handProcessor = HandProcessor(max_num_hands=1)
-
+handProcessor = HandProcessor(max_num_hands=2, min_detection_confidence=0.7)
 
 model = tf.keras.models.load_model("hands/hand_gesture_model")
 print(model.summary())
@@ -34,7 +37,7 @@ def write_text(frame, org, text, fg=(200, 200, 200), bg=(80, 0, 80)):
     cv.putText(
         frame,
         text,
-        (x, y - 20),
+        (x, y),
         cv.FONT_HERSHEY_PLAIN,
         fontScale=3,
         color=bg,
@@ -43,7 +46,7 @@ def write_text(frame, org, text, fg=(200, 200, 200), bg=(80, 0, 80)):
     cv.putText(
         frame,
         text,
-        (x, y - 20),
+        (x, y),
         cv.FONT_HERSHEY_PLAIN,
         fontScale=3,
         color=fg,
@@ -59,29 +62,38 @@ def main(cam_src=None):
     c = 0
     key = 0
     run = True
+    _, last_frame = cap.read()
+
     try:
         while run:
             c = c + 1
             has_frame, frame = cap.read()
             if has_frame:
+                frame = cv.flip(frame, 1)
                 h, w, c = frame.shape
-                handProcessor.proc(frame)
+                _ = handProcessor.proc(frame[:, :, ::-1])
+                for hand in handProcessor.get_handData():
+                    mpdUtils.draw_landmarks(
+                        image=frame,
+                        landmark_list=hand.multi_hand_landmarks,
+                        landmark_drawing_spec=None,
+                        connections=mpHands.HAND_CONNECTIONS,
+                    )
 
-                hands = handProcessor.get_landmark_as_np_arr()
-                for hand in hands:
-                    for (x1, y1) in hand:
-                        pt1 = (int(x1 * w), int(y1 * h))
-                        cv.circle(frame, pt1, 5, (200, 100, 0), -1)
-
+                for hand in handProcessor.get_landmark_as_np_arr():
                     current_gesture, probability = perdict_gesture(hand)
-
-                    pt = hand[4]
-                    (x, y) = (int(pt[0] * w), int(pt[1] * h))
-                    write_text(frame, (x, y), f"{current_gesture} %{probability}")
+                    center = hand.mean(axis=0)
+                    pt = (center * [w, h]).astype(np.int0)
+                    write_text(
+                        frame, pt + [-60, 15], f"{current_gesture} %{probability}"
+                    )
+                    # cv.circle(frame, pt, 10, (100, 0, 0), -1)
 
                 cv.imshow("main", frame)
 
-            waitKey = cv.waitKey(10)
+                last_frame = frame.copy()
+
+            waitKey = cv.waitKey(1)
             if waitKey > 0:
                 # print(waitKey, chr(waitKey))
                 if waitKey in [ord(str(i)) for i in range(10)]:
